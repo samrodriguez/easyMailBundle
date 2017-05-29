131eab64a8386acd48b3f28bc5fb75ca39efbc3f
samdeb <samdeb@samdeb>
2017-05-29 10:32:01 -0600

    Rename config values

--- src/ABC/EasyMailBundle/Controller/DefaultController.php
+++ src/ABC/EasyMailBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $mail = $this->get('easy.mailer');
-        $settings = array('default_theme'=>'other2',
+        $settings = array('default_theme'=>'other',
                           'to'=>'melitor@gmail.com',
                           /*
                           'cc'=>'myemail@email.com',
